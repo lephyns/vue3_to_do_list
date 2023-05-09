ba512ea37bb31fc608d365bf4151bc284158d00f
Rafael Rabelo da Silva <103958944+lephyns@users.noreply.github.com>
2023-05-09 18:52:40 -0300

    Aula 5 - Instruções de loops (diretiva v-for)

--- app/src/components/TheHeader.vue
+++ app/src/components/TheHeader.vue
@@ -1,18 +1,64 @@
 <template>
-    <header class="header">
-        Header
-    </header>
+<div>
+
+    <div v-for="(obj, index) in todos"
+    v-bind:key="obj.id"
+    class="todos-item"
+    >
+        {{ index }} - {{ obj.title }}
+    </div>
+
+
+</div>
 </template>
 
 <script>
     export default {
-
+        name: 'App',
+        data() {
+            return {
+                todos: [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  }
+]
+            }
+        }
     }
 </script>
 
 <style scoped>
-.header {
+.todos-item {
     background-color: #000;
+    margin: 0 0 5px 0;
+    padding: 3px 6px;
     color: #fff;
 }
 </style>
