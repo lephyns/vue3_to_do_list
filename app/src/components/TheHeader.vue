924c24e3c4d64b918cc5de2a83d2f9df05a382a1
rafael.silva <rafael.silva@deps.com.br>
2023-05-09 18:16:10 -0300

    Subindo projeto

---
+++ app/src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="header">
+        Header
+    </header>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+.header {
+    background-color: #000;
+    color: #fff;
+}
+</style>
